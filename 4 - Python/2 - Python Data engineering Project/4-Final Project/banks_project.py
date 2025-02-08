4e3a6b2534fe84303f0eb14ca600b3637da470e6
imadbenmadi <imadbenmadi891@gmail.com>
2025-02-08 13:07:01 +0100

    Refactor ETL process: remove outdated GDP data files, add new exchange rate data, and implement GDP extraction script

--- 4 - Python/2 - Python Data engineering Project/4-Final Project/banks_project.py
+++ 4 - Python/2 - Python Data engineering Project/4-Final Project/banks_project.py
@@ -4,14 +4,20 @@ import pandas as pd
 import numpy as np
 import sqlite3
 from datetime import datetime 
+import os
+
+url = 'https://web.archive.org/web/20230908091635%20/https://en.wikipedia.org/wiki/List_of_largest_banks'
+table_attribs = ["Name", "MC_USD_Billion"]
+final_table_attr = ["Name", "MC_USD_Billion","MC_GBP_Billion", "MC_EUR_Billion" ,"MC_INR_Billions"]
+output_csv_path = "Largest_banks_data.csv"
+db_name = 'Banks.db'
+table_name = 'Largest_banks'
+code_log = "code_log.txt"
 
 
-url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
-table_attribs = ["Country", "GDP_USD_millions"]
-table_name = 'Countries_by_GDP'
-csv_path = './3-Final Project/Countries_by_GDP.csv'
-db_name = './3-Final Project/World_Economies.db'
-etl_project_log = "./3-Final Project/etl_project_log.txt"
+
+
+
 
 
 
@@ -53,28 +59,28 @@ def extract(url, table_attribs):
         col = row.find_all('td')
         if len(col)!=0:
             if col[0].find('a') is not None and '—' not in col[2]:
-                data_dict = {"Country": col[0].a.contents[0],
-                             "GDP_USD_millions": col[2].contents[0]}
+                data_dict = {"Name": col[0].a.contents[0],
+                             "MC_USD_Billion": col[2].contents[0]}
                 df1 = pd.DataFrame(data_dict, index=[0])
                 df = pd.concat([df,df1], ignore_index=True)
     return df
 
 def my_transform(df):
     # Remove commas and dollar signs and convert to float
-    df['GDP_USD_millions'] = df['GDP_USD_millions'].str.replace(',', '').str.replace(r'\$', '').astype(float) 
+    df['MC_USD_Billion'] = df['MC_USD_Billion'].str.replace(',', '').str.replace(r'\$', '').astype(float) 
     # devide by 1000 and round to 2 decimal places
-    df['GDP_USD_millions'] = round(df['GDP_USD_millions']/1000, 2)
-    # Modify the name of the column from 'GDP_USD_millions' to 'GDP_USD_billions'
-    df.rename(columns={'GDP_USD_millions': 'GDP_USD_billions'}, inplace=True)
+    df['MC_USD_Billion'] = round(df['MC_USD_Billion']/1000, 2)
+    # Modify the name of the column from 'MC_USD_Billion' to 'GDP_USD_billions'
+    df.rename(columns={'MC_USD_Billion': 'GDP_USD_billions'}, inplace=True)
     log_progress("Data transformed successfully")
     return df
 
 def transform(df):
-    GDP_list = df["GDP_USD_millions"].tolist()
+    GDP_list = df["MC_USD_Billion"].tolist()
     GDP_list = [float("".join(x.split(','))) for x in GDP_list]
     GDP_list = [np.round(x/1000,2) for x in GDP_list]
-    df["GDP_USD_millions"] = GDP_list
-    df=df.rename(columns = {"GDP_USD_millions":"GDP_USD_billions"})
+    df["MC_USD_Billion"] = GDP_list
+    df=df.rename(columns = {"MC_USD_Billion":"GDP_USD_billions"})
     return df
 
 def my_load(df, csv_path):
@@ -90,7 +96,10 @@ def log_progress(message):
     timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
     now = datetime.now() # get current timestamp 
     timestamp = now.strftime(timestamp_format) 
-    with open(etl_project_log, 'a') as f:
+    if not os.path.exists(code_log):
+        with open(code_log, 'w') as f:
+            pass
+    with open(code_log, 'a') as f:
         f.write(timestamp + ' : ' + message + '\n')
 
 
